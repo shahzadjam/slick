2a3341c3de5a5695a1549b037565ec0a0a000dcf
Mohsin Abbas <mohsin.abbas@coeus-solutions.de>
2015-02-10 23:44:09 +0500

    feed fetcher added

---
+++ wp-content/plugins/social-feed-fetcher/social-feed-fetcher.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Social Feed Fetcher
+Plugin URI: http://slick.social/
+Description: Create a simple shortcode to list our WordPress users.
+Author: Mohsin
+Version: 1.6.1
+Author URI: http://slick.social
+License: GPL2
+*/
+
+
+
+?>
