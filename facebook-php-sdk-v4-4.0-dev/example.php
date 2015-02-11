7cf4f16a8706f95396dd37f3041e9ca4581e54ca
shahzad <shahzadjam18@gmail.com>
2015-02-11 23:20:35 +0500

    sdk

---
+++ facebook-php-sdk-v4-4.0-dev/example.php
@@ -0,0 +1,68 @@
+<?php
+
+// Skip these two lines if you're using Composer
+/*define('FACEBOOK_SDK_V4_SRC_DIR', '/src/Facebook/');
+require __DIR__ . '/autoload.php';*/
+session_start();
+ 
+ 
+require_once( 'Facebook/FacebookSession.php' );
+require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
+require_once( 'Facebook/FacebookRequest.php' );
+require_once( 'Facebook/FacebookResponse.php' );
+require_once( 'Facebook/FacebookSDKException.php' );
+require_once( 'Facebook/FacebookRequestException.php' );
+require_once( 'Facebook/FacebookAuthorizationException.php' );
+require_once( 'Facebook/GraphObject.php' );
+require_once( 'Facebook/Entities/AccessToken.php' );
+require_once( 'Facebook/HttpClients/FacebookHttpable.php' );
+require_once( 'Facebook/HttpClients/FacebookCurlHttpClient.php' );
+require_once( 'Facebook/HttpClients/FacebookCurl.php' ); 
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\Entities\AccessToken;
+use Facebook\HttpClients\FacebookHttpable;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+use Facebook\HttpClients\FacebookCurl;
+
+FacebookSession::setDefaultApplication('1033351130013312','45d781f240672235bc7acc27635ee73c');
+
+// Use one of the helper classes to get a FacebookSession object.
+//   FacebookRedirectLoginHelper
+//   FacebookCanvasLoginHelper
+//   FacebookJavaScriptLoginHelper
+// or create a FacebookSession with a valid access token:
+
+// login helper with redirect_uri
+$helper = new FacebookRedirectLoginHelper( 'http://localhost/facebook-php-sdk-v4-4.0-dev/example.php' );
+
+try {
+
+  $session = $helper->getSessionFromRedirect();
+} catch( FacebookRequestException $ex ) {
+  // When Facebook returns an error
+} catch( Exception $ex ) {
+  // When validation fails or other local issues
+}
+
+// see if we have a session
+if ( isset( $session ) ) {
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/me' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject();
+
+  // print data
+  echo  print_r( $graphObject, 1 );
+} else {
+  // show login url
+  echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
+}
